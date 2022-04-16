9e3964b890a966b3db0b4fc013998b4fe86d11ea
Dmitrii-Kh <actibagamma@gmail.com>
2022-04-16 10:05:01 +0300

    refactor app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OrganisationModule } from './organisation/organisation.module';
-import { StationModule } from './station/station.module';
-import { EacModule } from './eac/eac.module';
-import { MeasurementsModule } from './measurements/measurements.module';
-import { UserModule } from './user/user.module';
-import { WalletModule } from './wallet/wallet.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import {OrganisationModule} from './organisation/organisation.module';
+import {StationModule} from './station/station.module';
+import {EacModule} from './eac/eac.module';
+import {MeasurementsModule} from './measurements/measurements.module';
+import {UserModule} from './user/user.module';
+import {WalletModule} from './wallet/wallet.module';
+import {TokenModule} from './token/token.module';
 
 @Module({
-  imports: [OrganisationModule, StationModule, EacModule, MeasurementsModule, UserModule, WalletModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({isGlobal: true}), OrganisationModule, StationModule, EacModule, MeasurementsModule, UserModule, WalletModule, TokenModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
