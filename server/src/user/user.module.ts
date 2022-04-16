2b676e38c1f70535ca10a32c7567e4609c191845
Dmitrii-Kh <actibagamma@gmail.com>
2022-04-16 10:01:13 +0300

    add user and user utils modules

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import {FabricModule} from "../utils/fabric/fabric.module";
+import {UserUtilsModule} from "../utils/user/user.module";
 
 @Module({
+  imports: [FabricModule, UserUtilsModule],
   controllers: [UserController],
   providers: [UserService]
 })
