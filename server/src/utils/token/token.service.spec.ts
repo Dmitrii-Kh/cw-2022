176a5250e77d1f33717e3b3d376d6e4530fcc142
Dmitrii-Kh <actibagamma@gmail.com>
2022-04-16 10:03:41 +0300

    fix token.service.spec.ts

--- server/src/utils/token/token.service.spec.ts
+++ server/src/utils/token/token.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TokenService } from './token.service';
+import { TokenUtils } from './token.service';
 
 describe('TokenService', () => {
-  let service: TokenService;
+  let service: TokenUtils;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TokenService],
+      providers: [TokenUtils],
     }).compile();
 
-    service = module.get<TokenService>(TokenService);
+    service = module.get<TokenUtils>(TokenUtils);
   });
 
   it('should be defined', () => {
