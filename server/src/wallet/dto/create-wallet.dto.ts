69b50876cc8b0298b33a8cdc08f409288257e061
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-10 16:11:27 +0300

    init wallet module

--- server/src/wallet/dto/create-wallet.dto.ts
+++ server/src/wallet/dto/create-wallet.dto.ts
@@ -1,1 +1,15 @@
-export class CreateWalletDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateWalletDto {
+    @IsNumber()
+    @IsNotEmpty({message: 'UserId is required'})
+    userId: number;
+
+    @IsString()
+    @IsNotEmpty({message: 'Currency name is required'})
+    currency: string;
+
+    @IsNumber()
+    @IsNotEmpty({message: 'Balance is required'})
+    balance: number;
+}
--- server/src/wallet/entities/wallet.entity.ts
+++ server/src/wallet/entities/wallet.entity.ts
@@ -1,1 +1,12 @@
-export class Wallet {}
+import { PrimaryColumn, Column } from 'typeorm';
+
+export class Wallet {
+    @PrimaryColumn()
+    userId: number;
+
+    @PrimaryColumn()
+    currency: string;
+
+    @Column()
+    balance: number;
+}
