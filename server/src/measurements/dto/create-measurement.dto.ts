f9d12f5e31bd5aede5da6309ee84a22e3146fe7f
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-10 16:07:30 +0300

    init measurement module

--- server/src/measurements/dto/create-measurement.dto.ts
+++ server/src/measurements/dto/create-measurement.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMeasurementDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateMeasurementDto {
+    @IsNotEmpty({ message: 'Start Date should not be empty' })
+    @IsString()
+    startDate: string;
+
+    @IsNotEmpty({ message: 'End Date should not be empty' })
+    @IsString()
+    endDate: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    generatedEnergy: number;
+}
