f9e6e7c08dbbbb4b3143c14aaf7d640300feadb3
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-10 16:10:59 +0300

    add user entity, dto, role enum

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { UserRole } from '../../utils/userRole';
+
+export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty({message: 'Email is required'})
+    email: string;
+
+    @IsString()
+    fullName: string;
+
+    @IsString()
+    @IsNotEmpty({message: 'Password is required'})
+    password: string;
+
+    @IsNotEmpty({ message: 'User role is required' })
+    role: UserRole;
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from '../../utils/userRole';
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    fullName: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    salt: string;
+
+    @Column()
+    role: UserRole;
+}
