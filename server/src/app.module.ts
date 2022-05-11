75f26910120a0221f7397d38469e436f9a5fb978
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-11 11:52:42 +0300

    refactoring

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,14 +4,14 @@ import {AppService} from './app.service';
 import {ConfigModule} from '@nestjs/config';
 import {OrganisationModule} from './organisation/organisation.module';
 import {StationModule} from './station/station.module';
-import {EacModule} from './eac/eac.module';
+import {EacMarketModule} from './eac-market/eac-market.module';
 import {MeasurementsModule} from './measurements/measurements.module';
 import {UserModule} from './user/user.module';
 import {WalletModule} from './wallet/wallet.module';
 import {TokenModule} from './token/token.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({isGlobal: true}), OrganisationModule, StationModule, EacModule, MeasurementsModule, UserModule, WalletModule, TokenModule],
+    imports: [ConfigModule.forRoot({isGlobal: true}), OrganisationModule, StationModule, EacMarketModule, MeasurementsModule, UserModule, WalletModule, TokenModule],
     controllers: [AppController],
     providers: [AppService],
 })
