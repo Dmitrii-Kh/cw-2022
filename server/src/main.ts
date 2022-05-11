628382ee3246b5666de2e133d4f2e6c10f5eb26c
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-11 11:53:16 +0300

    add ctx subscription method

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,43 @@ import {AppModule} from './app.module';
 import {Logger} from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import axios from 'axios';
+
+async function subscribeToCtxChanges() {
+    try {
+        const response = await axios.get(
+            `${process.env.BROKER_HOST}:${process.env.BROKER_PORT}/v2/subscriptions`,
+        );
+        console.log(response.status);
+        const subs = response.data;
+        if (Array.isArray(subs) && subs.length === 0) {
+            const response = await axios.post(
+                `${process.env.BROKER_HOST}:${process.env.BROKER_PORT}/v2/subscriptions`,
+                {
+                    description: 'Notify backend of all context changes',
+                    subject: {
+                        entities: [
+                            {
+                                idPattern: '.*',
+                                type: 'Station',
+                            },
+                        ],
+                    },
+                    notification: {
+                        http: {
+                            url: `${process.env.BASE_URL}:${process.env.PORT}/api/measurements`,
+                        },
+                        attrsFormat: 'keyValues',
+                    },
+                    throttling: 1,
+                },
+            );
+            console.log(response);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 async function bootstrap() {
     const port = process.env.PORT;
@@ -27,6 +64,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('swagger', app, document);
 
+    // await subscribeToCtxChanges();
     await app.listen(port);
     logger.log(`Application listening on port ${port}`);
 }
