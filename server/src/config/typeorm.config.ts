aab5f628039013fa8929e676d9b7c0fb5332be8a
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-15 23:52:56 +0300

    update typeorm.config.ts

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,11 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Organisation } from '../organisation/entities/organisation.entity';
-import { Measurement } from '../measurements/entities/measurement.entity';
-import { Station } from '../station/entities/station.entity';
-import { EacMarketModule } from '../eac-market/eac-market.module';
-import { Wallet } from '../wallet/entities/wallet.entity';
-import { User } from '../user/entities/user.entity';
-
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
@@ -13,7 +6,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    database: 'cw-2022',
-    entities: [Organisation, Station, Measurement, Wallet, EacMarketModule, User],
+    database: 'cw2022',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 };
