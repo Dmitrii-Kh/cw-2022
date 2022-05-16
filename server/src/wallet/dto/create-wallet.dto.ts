200e25b6cfa5e9e706e5bafe9d70e2961a6aa02d
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-17 00:02:48 +0300

    refactor DTOs

--- server/src/wallet/dto/create-wallet.dto.ts
+++ server/src/wallet/dto/create-wallet.dto.ts
@@ -1,16 +1,21 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { FiatCurrencyEnum } from '../fiat-currency.enum';
+import { Exclude, Expose } from 'class-transformer';
 
+@Exclude()
 export class CreateWalletDto {
     @IsNumber()
     @IsNotEmpty({message: 'UserId is required'})
+    @Expose()
     userId: number;
 
     @IsString()
     @IsNotEmpty({message: 'Currency name is required'})
+    @Expose()
     currency: FiatCurrencyEnum;
 
     @IsNumber()
     @IsNotEmpty({message: 'Balance is required'})
+    @Expose()
     balance: number;
 }
