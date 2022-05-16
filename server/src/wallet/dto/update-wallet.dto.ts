8870be89b273d57632a295222ee4d79e93687dd0
Dmitrii-Kh <actibagamma@gmail.com>
2022-05-17 00:04:08 +0300

    refactor update DTOs

--- server/src/wallet/dto/update-wallet.dto.ts
+++ server/src/wallet/dto/update-wallet.dto.ts
@@ -2,18 +2,22 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateWalletDto } from './create-wallet.dto';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { FiatCurrencyEnum } from '../fiat-currency.enum';
+import { Exclude, Expose } from 'class-transformer';
 
+@Exclude()
 export class UpdateWalletDto extends PartialType(CreateWalletDto) {
     @IsNumber()
     @IsNotEmpty({message: 'UserId is required'})
+    @Expose()
     userId: number;
 
     @IsString()
     @IsNotEmpty({message: 'Currency name is required'})
+    @Expose()
     currency: FiatCurrencyEnum;
 
     @IsNumber()
     @IsNotEmpty({message: 'Amount is required'})
+    @Expose()
     amount: number;
-
 }
